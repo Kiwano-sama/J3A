d0adc45810afc1f8d7fd379af72518109bf81a1d
ikaslea <ikaslea@DESKTOP-UIFSPE6>
2017-09-28 09:19:30 +0200

    Cabecera separada con include

--- index.php
+++ index.php
@@ -34,26 +34,10 @@
         <!-- Container -->
         <section id="container">
         
-            <!-- Header -->
-			<header> 
-            	<!-- Logo -->
-            	<div id="logo">
-                	<h2>ANDERSON SMITH</h2>
-                    <h4>GRAPHIC & WEB DESIGNER</h4>
-                </div>
-                <!-- /Logo -->
-                
-                <!-- Social Icons -->
-                <ul class="socialicons">
-                    <li class="socials-text">Social Profiles</li>
-                	<li><a href="#" class="transition">&#x2a;</a></li>
-                    <li><a href="#" class="transition">&#x2c;</a></li>
-                    <li><a href="#" class="transition">&#x2d;</a></li>
-                    <li><a href="#" class="transition">&#x31;</a></li>
-                </ul>
-                <!-- /Social Icons -->
-            </header>
-            <!-- /Header -->
+          <?php 
+          include 'cabecera.php';
+          
+          ?>
             
             <!-- Content -->
             <section id="content" >
