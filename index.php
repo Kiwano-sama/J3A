660aee75a9352a3fcc8c93eb5f8aa3abbda46ddd
ikaslea <ikaslea@DESKTOP-UIFSPE6>
2017-09-28 09:27:18 +0200

    Prueba funcionamiento

--- index.php
+++ index.php
@@ -42,48 +42,12 @@
             <!-- Content -->
             <section id="content" >
             
-                <!-- Profile -->
-                <div id="profile"> 
-                 	<!-- About section -->
-                	<div class="about">
-                    	<div class="photo-inner">
-                            <ul>
-                                <li><img src="images/photo.jpg" height="186" width="153" /></li>
-                                <li><img src="images/photo1.png" height="186" width="153" /></li>
-                            </ul>
-                        </div>
-                        <h1>ANDERSON SMITH</h1>
-                        <h3>GRAPHIC & WEB DESIGNER</h3>
-                        <p>I like to make cool and creative designs. My design stash is always full of refreshing ideas. Feel free to take a look around my Vcard.</p>
-                    </div>
-                    <!-- /About section -->
-                     
-                    <!-- Personal info section -->
-                	<ul class="personal-info">
-						<li><label>Name</label><span>Anderson smith</span></li>
-                        <li><label>Birthday</label><span>March 13, 1988</span></li>
-                        <li><label>Address</label><span>Melbourne Victoria 3000 Australia</span></li>
-                        <li><label>Email</label><span class="word-wrap">Anderson.smith@gmail.com</span></li>
-                        <li><label>Phone</label><span>+123 456 789 111</span></li>
-                        <li><label>Website</label><span class="word-wrap"><a href="#">www.Andersonsmith.com</a></span></li>
-                    </ul>
-                    <!-- /Personal info section -->
-                </div>        
-                <!-- /Profile --> 
+		<?php 
+          include 'principal.php';
+          include 'menu.php';
+          ?>
 
-                <!-- Menu -->
-                <nav class="menu">
-                	<ul class="tabs">
-                        <li class="tmenu"><a href="#profile" class="tab-profile"><i>&#x43;</i>Profile</a></li>
-                        <li class="tmenu"><a href="#resume"><i>&#x3a;</i>Resume</a></li>
-                        <li class="tmenu"><a href="#portfolio"><i>&#x38;</i>Portfolio</a></li>
-                        <li class="tmenu"><a href="#contact"><i>&#x21;</i>Contact</a></li>
-                    </ul>
-                    <a class="prev" id="menu-prev" href="#">&#xe073;</a>
-                    <a class="next" id="menu-next" href="#">&#xe076;</a>
-
-                </nav>
-                <!-- /Menu --> 
+                 
                 
                 <!-- Resume -->
                 <section id="resume">
