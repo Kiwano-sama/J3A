f3f7b63508e05536b13571dccb0f2df40480b2c5
Valery <Valery@DESKTOP-UQNK6GM>
2017-09-28 10:16:33 +0200

    Cambio charset para carácteres españoles

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <head>
     
     <title>J3A - Diseñadores Gráficos y Web</title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" type="text/css" href="css/reset.css"/> 
