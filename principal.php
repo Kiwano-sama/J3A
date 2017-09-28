b173933d24db5353a8736d32ba8583aeb12dae5e
Valery <Valery@DESKTOP-UQNK6GM>
2017-09-28 10:19:43 +0200

    Actualziada informacion página principal

--- principal.php
+++ principal.php
@@ -8,20 +8,20 @@
                                 <li><img src="images/photo1.png" height="186" width="153" /></li>
                             </ul>
                         </div>
-                        <h1>ANDERSON SMITH</h1>
-                        <h3>GRAPHIC & WEB DESIGNER</h3>
+                        <h1>J3A</h1>
+                        <h3>GRAPHIC & WEB DESIGNERS</h3>
                         <p>I like to make cool and creative designs. My design stash is always full of refreshing ideas. Feel free to take a look around my Vcard.</p>
                     </div>
                     <!-- /About section -->
                      
                     <!-- Personal info section -->
                 	<ul class="personal-info">
-						<li><label>Name</label><span>Anderson smith</span></li>
-                        <li><label>Birthday</label><span>March 13, 1988</span></li>
-                        <li><label>Address</label><span>Melbourne Victoria 3000 Australia</span></li>
-                        <li><label>Email</label><span class="word-wrap">Anderson.smith@gmail.com</span></li>
-                        <li><label>Phone</label><span>+123 456 789 111</span></li>
-                        <li><label>Website</label><span class="word-wrap"><a href="#">www.Andersonsmith.com</a></span></li>
+						<li><label>Empresa</label><span>J3A S.A.</span></li>
+                        <li><label>Fundada</label><span>Septiembre 27, 2017</span></li>
+                        <li><label>Direc.</label><span>Zornotza, Urritxe 4 48150</span></li>
+                        <li><label>Email</label><span class="word-wrap">contact@j3a.com</span></li>
+                        <li><label>Phone</label><span>+34 616 789 111</span></li>
+                        <li><label>Website</label><span class="word-wrap"><a href="#">www.j3a.com</a></span></li>
                     </ul>
                     <!-- /Personal info section -->
                 </div>        
