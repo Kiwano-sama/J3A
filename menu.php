8df7823ba59fe28874f5beb9610291fae2098fd1
Jenn <Jenn@DESKTOP-EG1HEDI>
2017-09-28 14:31:26 +0200

    //modificaciones y creacion de css

--- menu.php
+++ menu.php
@@ -4,8 +4,8 @@
                         <li class="tmenu"><a href="#profile" class="tab-profile"><i>&#x43;</i>Portal</a></li>
                         <li class="tmenu"><a href="#resume"><i>&#x3a;</i>Qué Hacemos</a></li>
                         <li class="tmenu"><a href="#portfolio"><i>&#x38;</i>Nuestros trabajos</a></li>
-                        <li class="tmenu"><a href="#clientes"><i>&#x38;</i>Área clientes</a></li>
-                        <li class="tmenu"><a href="#trabajo"><i>&#x38;</i>Trabaja con nosotros</a></li>
+                        <li class="tmenu"><a href="#clientes"><i>&#x44;</i>Área clientes</a></li>
+                        <li class="tmenu"><a href="#trabajo"><i>&#xe17e;</i>Trabaja con nosotros</a></li>
                         <li class="tmenu"><a href="#contact"><i>&#x21;</i>Contacto</a></li>
                     </ul>
                     <a class="prev" id="menu-prev" href="#">&#xe073;</a>
