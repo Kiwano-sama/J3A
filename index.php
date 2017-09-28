923b3a3cf6bc7bde0cfb15c62014c58a5285280f
Valery <Valery@DESKTOP-UQNK6GM>
2017-09-28 10:34:29 +0200

    Seccion área cliente añadida

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
 	          include 'menu.php';
 	          include 'sobre.php';
 	          include 'nuestroTrabajo.php';
+	          include 'areaCliente.php';
 	          include 'contacto.php';
 	          ?>
 
