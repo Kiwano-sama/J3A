920dc2b76cbf6cb33ed84bae7ee371bdebeb5397
ikaslea <ikaslea@DESKTOP-UIFSPE6>
2017-09-28 11:02:24 +0200

    prueba li contacto

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
 			<footer>
-            	<div class="copyright"><a href="#contact">Contacta con nosotros.</a></div>
+            	<div class="copyright"><li class="tmenu"><a href="#contact">Contacta con nosotros.</a></li></div>
             </footer>
             <!-- /Footer --> 
