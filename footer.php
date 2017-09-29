6b5e8d2275f17aa822731e783110ed6154fce7a7
ikaslea <ikaslea@DESKTOP-UIFSPE6>
2017-09-29 09:26:31 +0200

    J3A Reborn

--- footer.php
+++
@@ -1,5 +0,0 @@
-<!-- Footer -->
-			<footer>
-            	<div class="copyright"><li class="tmenu"><a href="#contact">Contacta con nosotros.</a></li></div>
-            </footer>
-            <!-- /Footer --> 
