8fd2f705707b332a39f042e2233cdf695ad745ac
Jenn <Jenn@DESKTOP-EG1HEDI>
2017-09-29 08:51:31 +0200

    //cambio en icono menu area clientes

--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@
                         <li class="tmenu"><a href="#profile" class="tab-profile"><i>&#x43;</i>Portal</a></li>
                         <li class="tmenu"><a href="#resume"><i>&#x3a;</i>Qué Hacemos</a></li>
                         <li class="tmenu"><a href="#portfolio"><i>&#x38;</i>Nuestros trabajos</a></li>
-                        <li class="tmenu"><a href="#clientes">&#x44;<i></i>Área clientes</a></li>
+                        <li class="tmenu"><a href="#clientes"><i>&#x44;</i>Área clientes</a></li>
                         <li class="tmenu"><a href="#trabajo"><i>&#xe17e;</i>Trabaja con nosotros</a></li>
                         <li class="tmenu"><a href="#contact"><i>&#x21;</i>Contacto</a></li>
                     </ul>
