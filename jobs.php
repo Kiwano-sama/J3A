51f01b685412f57a6db0f4c8f3ca4366bde7d2e1
Jenn <Jenn@DESKTOP-EG1HEDI>
2017-09-29 13:49:46 +0200

    //modificaciones pagina jobs.php

--- jobs.php
+++ jobs.php
@@ -1,11 +1,98 @@
-<html>
-    <head>
-        <link href="https://fonts.googleapis.com/css?family=Oswald:700|Patua+One|Roboto+Condensed:700" rel="stylesheet">
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"> 
-        <link href="css/style.css" rel="stylesheet" type="text/css"/>
-    </head>
-    <body align="center" >
-        <section  id="contact" class="content-section text-center">
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="canonical" href="http://html5-templates.com/" />
+    <title>J3A-Home</title>
+	<meta name="description" content="A minimalist Bootstrap theme by StartBootstrap. Contains everything you need to get started building your website. All you have to do is change the text and images.">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">J3A-Web Developers & Designers</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="about.php">Sobre Nosotros</a>
+                    </li>
+                    <li>
+                        <a href="services.php">Servicios</a>
+                    </li>
+                     <li>
+                        <a href="jobs.php">Trabaja con Nosotros</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contáctanos</a>
+                    </li>
+                    <li>
+                        <a href="portfolio-3-col.html">Ejemplos de Proyectos</a>
+                    </li>
+                            
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Header Carousel -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="fill" style="background-image:url('http://www.in-tuition.net/wp-content/uploads/2015/11/Malware.png');"></div>
+                <div class="carousel-caption">
+                    <h3>Desarrollamos aplicaciones para empresas</h3>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('https://holykaw.alltop.com/wp-content/uploads/2013/11/Screen-Shot-2013-11-12-at-2.08.19-PM.png');"></div>
+                <div class="carousel-caption">
+                    <h3>Diseñamos tu web a tu gusto</h3>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://avanzamarketing.com/wp-content/uploads/2016/12/pantallazoazul-1024x285.jpg');"></div>
+                <div class="carousel-caption">
+                    <h3>Realizamos mantenimientos de páginas y apps</h3>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </header>
+
+    <!-- Page Content -->
+<section  id="contact" class="content-section text-center">
             <div class="contact-section">
                 <div class="container">
                     <h1>Quieres trabajar con nosotros?</h1>
@@ -84,8 +171,34 @@
                 </div>
              </div>
         </section>
-    <footer>
-           <p>Copyright © NuestraEmpresa | <a href="http://www.susdatos.es/proteccion-datos-documentos-gratis/documentos/modelos-de-clausulas-para-paginas-web/modelo-de-politica-de-privacidad-para-pagina-web-con-formulario" target="_blank" >Privacy Policy</a> | <a href="https://formaldocs.com/2014/12/Modelo-de-condiciones-de-uso-de-una-pagina-web" target="_blank">Terms of Use</a></p>
-    </footer>
-  </body>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website, year | <a href="http://html5-templates.com/" target="_blank" rel="nofollow">HTML5 Templates</a></p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
 </html>
+
+
