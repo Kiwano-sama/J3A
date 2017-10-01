0787a917614d440b44d2c3dcd7a6a0038803a5dd
savANDY <andicuu@gmail.com>
2017-10-01 18:37:56 +0200

    Subido tema nuevo

---
+++ new/header.php
@@ -0,0 +1,74 @@
+    <header>
+        <div class="container">
+            <div class="row hidden-phone">
+                <img class="flip" src="assets/img/panel.jpg" style="float: right;"/><span class="header_social">Siguenos en <span class="badge"><a href="#">Twitter</a></span> y <span class="badge"><a href="#">Facebook</a></span></span>
+            </div>
+            <div class="row">
+                <div class="span4 logo">
+                    <a href="index.php"><img src="assets/img/logo.png" alt="logo" style="margin-bottom:7px; margin-top:7px;" /></a>
+                </div>
+                <div class="span8">
+                    <nav id="main-nav">
+                        <ul id="menu">
+                            <li class="current"><a href="index.php">Portal</a>
+                                <ul>
+                                    <li><a href="index.php">Default</a></li>
+                                    <li><a href="index_compact.html">Compact</a></li>
+                                </ul>
+                            </li>
+                            <!--<li class="with_icon"><a href="#"><i class="icon-exclamation-sign"></i> Features</a>
+                                <ul>
+                                    <li><a href="scaffolding.html">Scaffolding</a></li>
+                                    <li><a href="#">Base CSS</a>
+                                        <ul>
+                                            <li><a href="typography_code.html">Typography and Code</a></li>
+                                            <li><a href="tables_forms.html">Tables and Forms</a></li>
+                                            <li><a href="buttons_icons.html">Buttons and Icons</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Components</a>
+                                        <ul>
+                                            <li><a href="navigation.html">Navigation</a></li>
+                                            <li><a href="labels_badges.html">Labels and Badges</a></li>
+                                            <li><a href="alerts_bars.html">Alerts and Bars</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Javascript plugins</a>
+                                        <ul>
+                                            <li><a href="java_i.html">Pack I</a></li>
+                                            <li><a href="java_ii.html">Pack II</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>-->
+                            <li><a href="sobrenosotros.php">Sobre nosotros</a></li>
+                            <li><a href="servicios.php">Qué hacemos</a></li>
+                            <li><a href="miembros.php">Miembros</a> 
+                                <ul>
+                                    <li><a href="miembros.php">Ver miembros</a></li>
+                                    <li><a href="miperfil.php">Mi perfil</a></li>
+                                    <li><a href="ajustes.php">Ajustes</a></li>
+                                </ul>
+                            </li>
+                            <!--<li><a href="blog_i.html">Blog</a>
+                                <ul>
+                                	<li><a href="blog_i.html">Style I</a></li>
+                                    <li><a href="blog_ii.html">Style II</a></li>
+                                    <li><a href="blog_single.html">Single post</a></li>
+                                </ul>
+                            </li>-->
+                         <!--   <li><a href="4columns_portfolio.html">Portfolio</a>
+                            	<ul>
+                                    <li><a href="2columns_portfolio.html">2 Columns</a></li>
+                                    <li><a href="3columns_portfolio.html">3 Columns</a></li>
+                                    <li><a href="4columns_portfolio.html">4 Columns</a></li>
+                                    <li><a href="single_portfolio.html">Single Project</a></li>
+                                </ul>
+                            </li>-->
+                            <li><a href="contacto.php">Contacto</a></li>
+                        </ul>
+                    </nav><!-- end #main-nav -->
+                </div>
+            </div>
+		</div>
+	</header>
