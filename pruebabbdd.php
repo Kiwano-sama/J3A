ee44888e53cb6d1f08587611303a5fef1cc950cb
Valery <Valery@DESKTOP-UQNK6GM>
2017-10-02 10:04:32 +0200

    Prueba bbdd

--- pruebabbdd.php
+++ pruebabbdd.php
@@ -10,7 +10,7 @@
         $opcionElegida = $_POST['opcion'];
 
         function hacerConexion() {
-            return mysqli_connect('localhost', 'j3a', 'Qwerty123_', 'usuarios_bbdd');
+            return mysqli_connect('j3a.mysql.database.azure.com', 'j3a', 'Qwerty123_', 'usuarios_bbdd');
         }
 
         $link = hacerConexion();
