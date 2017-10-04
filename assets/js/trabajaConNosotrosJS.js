487103447b9acd073274a1f45389f6197b187ab4
jennerys13 <jennsflow19@gmail.com>
2017-10-04 09:52:07 +0200

    //modif js trabajaConNosotros

--- assets/js/trabajaConNosotrosJS.js
+++ assets/js/trabajaConNosotrosJS.js
@@ -29,7 +29,7 @@ function comprobarNombre() {
 		alert("Por favor, introduzca su nombre");
 		return false;
 	}
-	  if (/^\D/.test(miNombre) && (miNombre.length)){
+	  if (/^\D{20}/.test(miNombre) && (miNombre.length)){
 	        return true;
 	    }else {
 	        alert("El numero insertado " + miNumero + " es erroneo");
