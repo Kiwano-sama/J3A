798779ff5debac8891ef8f5cf8f565cc5a30d20a
savANDY <andicuu@gmail.com>
2017-10-05 09:07:30 +0200

    Cambio ruta logueo y ruta css

--- login/registro.php
+++ login/registro.php
@@ -5,7 +5,7 @@
         <title>J3A - Registro</title>
 
         <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-        <link href="assets/css/estiloRegistro.css" rel="stylesheet">
+        <link href="../assets/css/estiloRegistro.css" rel="stylesheet">
     </head>
     <body>
 
@@ -67,7 +67,7 @@
                     // check for successfull registration
                     if ($stmt->rowCount() == 1) {
                         
-                        echo ('<p>Registrado correctamente. <a href="login.php">Ya te puedes loguear</a>.</p>');
+                        echo ('<p>Registrado correctamente. <a href="./index.html">Ya te puedes loguear</a>.</p>');
                     } else {
                         echo ('<p class="texto-rojo">No se pudo registrar, intentalo de nuevo más tarde.</p>');
                     }
