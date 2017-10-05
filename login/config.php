6186ae00787003e641fb1d77fe10153f63f40a2c
savANDY <andicuu@gmail.com>
2017-10-05 09:04:53 +0200

    Conexion BBDD

---
+++ login/config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DBhost', '127.0.0.1:56624');
+define('DBuser', 'j3a');
+define('DBPass', 'Qwerty123_');
+define('DBname', 'j3a');
+
+try{
+	
+	$DB_con = new PDO("mysql:host=".DBhost.";dbname=".DBname,DBuser,DBPass);
+	
+}catch(PDOException $e){
+	
+	die($e->getMessage());
+}
