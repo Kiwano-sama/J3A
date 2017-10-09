38dafb7ff293e7a3a6430329317b5e0711d45349
savANDY <andicuu@gmail.com>
2017-10-09 11:08:26 +0200

    Test

--- miperfil.php
+++ miperfil.php
@@ -116,7 +116,7 @@ if($now > $_SESSION['expire']) {
       	<td>
       	<div class="profile-usertitle">
 					<div class="profile-usertitle-name">
-						Savandy
+						<?php echo ($_SESSION['username']);?>
 					</div>
 					<div class="profile-usertitle-job">
 						Administrador
@@ -156,65 +156,7 @@ if($now > $_SESSION['expire']) {
 </div>
   </section>
   
-  
-  <div class="container">
-    <div class="row profile">
-		<div class="col-md-3">
-			<div class="profile-sidebar">
-				<!-- SIDEBAR USERPIC -->
-				<div class="profile-userpic">
-					<img src="http://keenthemes.com/preview/metronic/theme/assets/admin/pages/media/profile/profile_user.jpg" class="img-responsive" alt="">
-				</div>
-				<!-- END SIDEBAR USERPIC -->
-				<!-- SIDEBAR USER TITLE -->
-				<div class="profile-usertitle">
-					<div class="profile-usertitle-name">
-						Marcus Doe
-					</div>
-					<div class="profile-usertitle-job">
-						Developer
-					</div>
-				</div>
-				<!-- END SIDEBAR USER TITLE -->
-				
-				<!-- SIDEBAR MENU -->
-				<div class="profile-usermenu">
-					<ul class="nav">
-						<li class="active">
-							<a href="#">
-							<i class="glyphicon glyphicon-home"></i>
-							Overview </a>
-						</li>
-						<li>
-							<a href="#">
-							<i class="glyphicon glyphicon-user"></i>
-							Account Settings </a>
-						</li>
-						<li>
-							<a href="#" target="_blank">
-							<i class="glyphicon glyphicon-ok"></i>
-							Tasks </a>
-						</li>
-						<li>
-							<a href="#">
-							<i class="glyphicon glyphicon-flag"></i>
-							Help </a>
-						</li>
-					</ul>
-				</div>
-				<!-- END MENU -->
-			</div>
-		</div>
-		<div class="col-md-9">
-            <div class="profile-content">
-			   Some user related content goes here...
-            </div>
-		</div>
-	</div>
-</div>
-  
-  
-  
+ 
   
   </div>
     <!--/AREA CONTENIDO-->
