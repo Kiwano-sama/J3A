54b3fd35f95c089dbf4544a5c7ddc5ee0c7bcaca
jennerys13 <jennsflow19@gmail.com>
2017-10-09 12:06:50 +0200

    //modif trabajaConNosotros php y js

--- trabajaConNosotros.php
+++ trabajaConNosotros.php
@@ -13,7 +13,7 @@
     <link href="assets/css/docs.css" rel="stylesheet">
     <link href="assets/css/cssTrabajaConNosotros.css" rel="stylesheet">
     <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/prettyPhoto.css" type="text/css" media="screen" title="prettyPhoto main stylesheet" />
+    <link rel="stylesheet" href="assets/css/prettyPhoto.css" type="text/css" media="screen" title="prettyPhoto main stylesheet"  />
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
