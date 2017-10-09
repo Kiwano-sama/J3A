1b9c4d85413461a573e7122376e284faeaf143df
savANDY <andicuu@gmail.com>
2017-10-09 10:21:12 +0200

    test

--- miperfil.php
+++ miperfil.php
@@ -109,8 +109,8 @@ if($now > $_SESSION['expire']) {
       	<td>
       	<!-- BOTONES SIDEBAR -->
 				<div class="profile-userbuttons">
-					<button type="button" class="btn btn-success btn-sm">Añadir</button>
-					<button type="button" class="btn btn-danger btn-sm">Mensaje</button>
+					<button type="button" class="btn btn-success btn-mini">Añadir</button>
+					<button type="button" class="btn btn-danger btn-mini">Mensaje</button>
 				</div>
 				<!-- /BOTONES SIDEBAR -->
       	</td>
