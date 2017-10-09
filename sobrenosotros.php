a75467ce5872af5d9fc07601bc39646ef9f8202f
Kiwano-sama <zharkenheim@gmail.com>
2017-10-09 10:37:01 +0200

    Fotos 400x400

--- sobrenosotros.php
+++ sobrenosotros.php
@@ -108,7 +108,6 @@
                                     <p style="margin-top:20px;"><a class="btn btn-success btn-small" style="margin-right:10%" href="#"><i class="icon-picture icon-white"></i> Ver  Curriculum</a></p>
                     </div>
            <h2>Ander</h2>
-                
                     <div class="span4">
 
 									<img alt="" src="img\Ander.png">
