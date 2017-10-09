3135d088ca61d0295f588f0a107879b24ce90f95
savANDY <andicuu@gmail.com>
2017-10-09 09:57:48 +0200

    test

--- miperfil.php
+++ miperfil.php
@@ -108,7 +108,7 @@ if($now > $_SESSION['expire']) {
   			<td><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">Perfil</a></td>
   			</tr>
   			<tr>
-  			<td>Amigos</td>
+  			<td><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">Amigos</a></td>
   			</tr>
   			<tr>
   			<td>Configuración</td>
@@ -121,11 +121,19 @@ if($now > $_SESSION['expire']) {
     </div>
     <div class="span10">
      <!--Contenido Perfil-->
-     <div id="collapseOne" class="accordion-body collapse in">
+     		  <div id="collapseOne" class="accordion-body collapse in">
                 <div class="accordion-inner">
                   Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                 </div>
               </div>
+              
+              <div id="collapseTwo" class="accordion-body collapse">
+                <div class="accordion-inner">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+              </div>
+              
+              
     </div>
   </div>
 </div>
