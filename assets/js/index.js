c070570eb9bec429e3e565c23d1284129678435b
Kiwano-sama <zharkenheim@gmail.com>
2017-10-10 12:30:40 +0200

    prueba paleta

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,11 @@
 function cambiarColor() {
+	document.getElementByTagName("p").style.color = "black";
 	// se define la variable del color de la paleta y la variable del valor de
 	// los radio button
 	var colorElegido = document.formPaleta.paleta.value;
-	var Ps = document.getElementsByTagName('p');
-
-	case 'etiquetaP':// con este case hacemos que todos los parrafos con la
-					// etiqueta 'p' cambie de color
-		// si son todos unamos tagname
-		var Ps = document.getElementsByTagName('p');
+	var Ps = document.getElementsByTagName("p");
 		// Ps[i].style.color = colorElegido;
-		for (var i = 0; i < Ps.length; i++)
-			Ps[i].style.color = colorElegido;
+	for (var i = 0; i < Ps.length; i ++)
+		Ps[i].style.color = colorElegido;
 	
 }
