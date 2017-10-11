cfd4a9f504269e41fa90aa446ca3633a3bb87de3
savANDY <andicuu@gmail.com>
2017-10-11 08:49:03 +0200

    Arreglo recoger informacion de bbdd

--- miperfil.php
+++ miperfil.php
@@ -104,7 +104,7 @@ $username = $_SESSION['username'];
 $sqlInfoUsuario = "SELECT Nombre, Apellido, fechaNac, genero, esCliente, esAdmin FROM `usuario` WHERE username = '$username'";
 $resultInfoUsuario = mysqli_query($link, $sql);
 
-if (mysqli_num_rows($resultInfoUsuario) > 0) {
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))  {
 	$nombre = $row['Nombre'];
 	$apellido = $row['Apellido'];
 	$fechaNac = $row['fechaNac'];
