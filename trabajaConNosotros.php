ccd0d6255777ae073834b49fe5fb770daa641735
jennerys13 <jennsflow19@gmail.com>
2017-10-11 09:27:58 +0200

    //modif formu php trabajaConNosotros

--- trabajaConNosotros.php
+++ trabajaConNosotros.php
@@ -122,7 +122,7 @@ include 'header.php';
                             <br>
                              <!-- Buttons -->
                          
-                         <input onSubmit="return Enviar()" type="submit" value="Enviar" class="btn-large btn-success mega" style="width:250px">
+                         <input  onSubmit="return Enviar()" type="submit" value="Enviar" class="btn-large btn-success mega" style="width:400px">
                          
                         </form>
                         <!-- form end -->
