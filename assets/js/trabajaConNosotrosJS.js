42561c0692becd7b117d9e48716b6743aa691a4f
jennerys13 <jennsflow19@gmail.com>
2017-10-16 13:23:23 +0200

    //pruebas js formulario

--- assets/js/trabajaConNosotrosJS.js
+++ assets/js/trabajaConNosotrosJS.js
@@ -22,69 +22,69 @@ function validarEmail() {
 		return false;
 	}
 }
-///* 	
-// * Funcion para determinar que parametros tiene que tener el 'Numero de
-// * contacto' y que debe tener 9 numeros si no da error y que no se pueda
-// * insertar letras en vez de numeros en 'Numero de
-// * contacto'
-//*/
-function comprobarNumero(e) {
-	var keychar;
-	var teclanum;
-	var miNumero = document.getElementById('numero');
-	// alert('holaa prueba 2 comprobarNumero');
-	teclanum = e.which;
-	keychar = String.fromCharCode(teclanum);
-
-	// alert('holaa prueba comprobarNumero');
-	if (keychar < '0' || keychar > '9') {
-		// alert('holaa prueba 1 comprobarNumero');
-		return false;
-	} else {
-		return true;
-	}
-	// alert('holaa prueba 2 comprobarNumero');
-	if (/^\d{9}/.test(miNumero) && (miNumero.length <= 9)) {
-		return true;
-	} else {
-		alert("El numero insertado " + miNumero + " es erroneo");
-		return false;
-	}
-}
-//Funcion para que no se puedan insertar numeros en el campo 'Nombre'
-function comprobarLetra(e) {
-	var keychar;
-	var teclanum;
-
-	teclanum = e.which;
-	keychar = String.fromCharCode(teclanum);
-
-	// alert(keychar);
-	if ((keychar >= 'a' && keychar <= 'z') || (keychar >= 'A' && keychar <= 'Z')) {
-		return true;
-	} else {
-		return false;
-	}
-}
-// // alert('holaa prueba 2 comprobarNumero');
-// }
-// Funcion para que no se pueda insertar mas de una '@'
-function comprobarArroba(e) {
-	var caracterTecla;
-	var codigoTecla;
-
-	if (window.event) {
-		codigoTecla = e.keyCode;
-	} else if (e.which) {
-		codigoTecla = e.which;
-	}
-	caracterTecla = String.fromCharCode(codigoTecla);
-
-	if (caracterTecla == "@") {
-		if (document.trabaja.email.value.indexOf('@') >= 0) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-}
+/////* 	
+//// * Funcion para determinar que parametros tiene que tener el 'Numero de
+//// * contacto' y que debe tener 9 numeros si no da error y que no se pueda
+//// * insertar letras en vez de numeros en 'Numero de
+//// * contacto'
+////*/
+//function comprobarNumero(e) {
+//	var keychar;
+//	var teclanum;
+//	var miNumero = document.getElementById('numero');
+//	// alert('holaa prueba 2 comprobarNumero');
+//	teclanum = e.which;
+//	keychar = String.fromCharCode(teclanum);
+//
+//	// alert('holaa prueba comprobarNumero');
+//	if (keychar < '0' || keychar > '9') {
+//		// alert('holaa prueba 1 comprobarNumero');
+//		return false;
+//	} else {
+//		return true;
+//	}
+//	// alert('holaa prueba 2 comprobarNumero');
+//	if (/^\d{9}/.test(miNumero) && (miNumero.length <= 9)) {
+//		return true;
+//	} else {
+//		alert("El numero insertado " + miNumero + " es erroneo");
+//		return false;
+//	}
+//}
+////Funcion para que no se puedan insertar numeros en el campo 'Nombre'
+//function comprobarLetra(e) {
+//	var keychar;
+//	var teclanum;
+//
+//	teclanum = e.which;
+//	keychar = String.fromCharCode(teclanum);
+//
+//	// alert(keychar);
+//	if ((keychar >= 'a' && keychar <= 'z') || (keychar >= 'A' && keychar <= 'Z')) {
+//		return true;
+//	} else {
+//		return false;
+//	}
+//}
+//// // alert('holaa prueba 2 comprobarNumero');
+//// }
+//// Funcion para que no se pueda insertar mas de una '@'
+//function comprobarArroba(e) {
+//	var caracterTecla;
+//	var codigoTecla;
+//
+//	if (window.event) {
+//		codigoTecla = e.keyCode;
+//	} else if (e.which) {
+//		codigoTecla = e.which;
+//	}
+//	caracterTecla = String.fromCharCode(codigoTecla);
+//
+//	if (caracterTecla == "@") {
+//		if (document.trabaja.email.value.indexOf('@') >= 0) {
+//			return false;
+//		} else {
+//			return true;
+//		}
+//	}
+//}
