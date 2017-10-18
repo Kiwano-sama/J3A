0dccaf390bf2ca3879a1ef21e0d8cabc9a56f8da
jennerys13 <jennsflow19@gmail.com>
2017-10-18 12:57:47 +0200

    //modif varias bbdd trabaja

--- bbdd_trabaja.php
+++ bbdd_trabaja.php
@@ -109,7 +109,7 @@ mysqli_close($conexion);
  
   <p>Los datos han sido guardados con éxito, uno de nuestros responsables se pondrá en contacto con usted :)
   </p>
-   <input src="http://j3a.azurewebsites.net/index.php"" type="submit" value="Volver al Portal" class="btn-large btn-success mega" style="width:350px">
+   <input src="http://j3a.azurewebsites.net/index.php" type="submit" value="Volver al Portal" class="btn-large btn-success mega" style="width:350px">
  	<!-- <meta http-equiv="acción"; content="segundos"; url="http://j3a.azurewebsites.net/index.php"/> -->
  
 <!-- <input href="index.php" type="submit" value="Volver" class="btn-large btn-success mega" tyle="width:350px"> -->
