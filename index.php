b8c10831c6bccb8e5992a1e275d013d5ac407be7
savANDY <andicuu@gmail.com>
2017-10-19 13:05:31 +0200

    Centrado boton y texto

--- index.php
+++ index.php
@@ -73,8 +73,8 @@ include 'header.php';
         </div>
     </div>
     <fieldset>
-    	<p style="align:center;" class="intro">¿Te cuesta ver el tamaño de la letra? Pulsa aquí para aumentar el tamaño de la fuente.</p>
-			<input style="align:center;" name="botonModificar" type="button" value="modificar Tamaño" onclick="Tamano()"/>	
+    	<p style="text-align:center;" class="intro">¿Te cuesta ver el tamaño de la letra? Pulsa aquí para aumentar el tamaño de la fuente.</p>
+			<p style="text-align:center;"><input name="botonModificar" type="button" value="modificar Tamaño" onclick="Tamano()"/>	</p>
    	</fieldset>        
     <!--/AREA BIENVENIDA-->
     <!--AREA SLIDER-->
