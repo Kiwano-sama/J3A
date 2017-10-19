8424abe30b7e083749d640be3da6f5cd61910963
jennerys <jennerys@jenn>
2017-10-19 08:10:55 +0200

    //modif varias php trabaja

--- bbdd_trabaja.php
+++ bbdd_trabaja.php
@@ -109,7 +109,7 @@ mysqli_close($conexion);
  
   <p>Los datos han sido guardados con éxito, uno de nuestros responsables se pondrá en contacto con usted :)
   </p>
-   <button src="index.php" value="Volver al Portal" class="btn-large btn-success mega" style="width:350px"/>
+   <button src="index.php" value="Volver al Portal" class="btn-large btn-success mega" style="width:350px"></button>
  	<!-- <meta http-equiv="acción"; content="segundos"; url="http://j3a.azurewebsites.net/index.php"/> -->
  
 <!-- <input href="index.php" type="submit" value="Volver" class="btn-large btn-success mega" tyle="width:350px"> -->
